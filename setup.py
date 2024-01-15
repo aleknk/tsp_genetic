e787d9b3d6c95d50e772c2e242f2c70010fff31c
aleknk <ale.kanka.dev@gmail.com>
2024-01-15 16:49:03 +0100

    setup file updated with entrypoints

--- setup.py
+++ setup.py
@@ -3,5 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name='pytsp',
     version='1.0',
-    packages=find_packages()
+    packages=find_packages(),
+    entry_points={
+            'console_scripts':[
+                'tspga_run = pytsp.tools.run:main',
+                'tspga_multirun = pytsp.tools.multirun:main'
+            ]
+        }
 )
